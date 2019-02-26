733f35c9c72af030d05ddef6526c1224ee951b0b
SarpongAbasimi <37377831+SarpongAbasimi@users.noreply.github.com>
2019-02-26 13:42:32 +0000

    Update and rename Numpy to Numpy.py

---
+++ Numpy.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+#Using Numpy
+test_1 = np.array([92, 94, 88, 91, 87])
+test_2 = np.array([79, 100, 86, 93, 91])
+test_3 = np.array([87, 85, 72, 90, 92])
+test_3_fixed = test_3 + 2
+
+
+total_grade= test_1 +test_2 + test_3_fixed 
+final_grade = total_grade // 3
+
+
+#Using List
+b=  zip(test_1,test_2 ,test_3)
+print(b)
+
+a= []
+
+for i in range(len(b)):
+  c=(sum(b[i])) // len(b[i])
+  a.append(c)
+
+#Printing Answers
+print(final_grade)
+print(a)
+
+""" 
+Percentiles
+
+As we know, the median is the middle of a dataset: 
+it is the number for which 50% of the samples are below, and 50% of the samples are above. 
+But what if we wanted to find a point at which 40% of the samples are below, and 60% of the samples are above?
+
+This type of point is called a percentile. 
+The Nth percentile is defined as the point N% of samples lie below it. 
+So the point where 40% of samples are below is called the 40th percentile.
+Percentiles are useful measurements because they can tell us where a particular value is situated within the greater dataset.
+
+In NumPy, we can calculate percentiles using the function np.percentile,
+which takes two arguments: the array and the percentile to calculate.
+
+"""
